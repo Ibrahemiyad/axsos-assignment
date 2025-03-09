55de1e52fa97b76cdf04fe5bfee73024ee64511e
ibrahem deek <ibrahem.deek@gmail.com>
2025-03-09 16:33:08 +0530

    tryibg to fix

---
+++ web/js/button clicker/button clicker .js
@@ -0,0 +1,9 @@
+function omclick(x){
+    x.innerText="logout"
+}
+function hide(m){
+    m.remove()
+}
+function show(){
+    alert("Ninja was liked")
+}
---
+++ web/js/video preview/script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementsByClassName("video");
+      function play(){
+      x.play()
+  }
+  function pause(){
+  x.pause()
+  }
+}  
+
